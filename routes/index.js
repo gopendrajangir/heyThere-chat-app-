0a775fa180345ae99b86bf39bac54de81e831a88
gopendra <gopendra1.kumar@gmail.com>
2018-08-04 16:29:12 +0530

    added autoscroll feature

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,14 @@ router.get('/',function(req, res){
   res.render('cover');
 });
 
+router.get('/join',function(req, res){
+  res.render('join');
+});
+
 router.get('/interface',function(req, res){
   res.render('interface');
 });
+
+
 
 module.exports = router;
